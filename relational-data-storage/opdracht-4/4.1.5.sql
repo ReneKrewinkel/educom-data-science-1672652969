e6183fd79e381d058c1e256bc244fe8a7b6324d7
Stef Lipka <steflipka021@gmail.com>
2023-01-11 10:53:46 +0100

    met inner join snel

--- relational-data-storage/opdracht-4/4.1.5.sql
+++ relational-data-storage/opdracht-4/4.1.5.sql
@@ -1,14 +1,14 @@
+-- Deze is veel sneller met INNER/LEFT joins
+SELECT name, straat, huisnr, postcode, LL1.lat, LL1.lng
+FROM mhl_suppliers S
+INNER JOIN pc_lat_long LL1 ON S.postcode = LL1.pc6
+ORDER BY LL1.lat DESC
+LIMIT 5;
+
+
+-- Oude langzame query:
 SELECT name, straat, huisnr, postcode, lat, lng
 FROM mhl_suppliers S
 RIGHT JOIN pc_lat_long ON S.postcode = pc_lat_long.pc6
 ORDER BY lat DESC
-LIMIT 5;
-
-
-
-SELECT name, straat, huisnr, postcode, LL1.lat, LL1.lng, LL2.lat, LL2.lng
-FROM mhl_suppliers S
-INNER JOIN pc_lat_long LL1 ON S.postcode = LL1.pc6
-INNER JOIN pc_lat_long LL2 ON S.p_postcode = LL2.pc6
-ORDER BY LL1.lat DESC, LL2.lat DESC
 LIMIT 5;
