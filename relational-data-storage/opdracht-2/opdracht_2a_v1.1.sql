c31f6011a26ace57a8e435abb0213c76a17f988d
Stef Lipka <steflipka021@gmail.com>
2023-01-23 17:01:21 +0100

    foreign keys aanmaken opdr2.1a

--- relational-data-storage/opdracht-2/opdracht_2a_v1.1.sql
+++ relational-data-storage/opdracht-2/opdracht_2a_v1.1.sql
@@ -27,10 +27,17 @@ DROP FOREIGN KEY FK_mhl_districts_country_ID;
 
 -- CREATE FK ----------------------------------------------------------------
 
+-- 1. mhl_cities EN mhl_communes ==============================================
+
 ALTER TABLE mhl_cities
 ADD CONSTRAINT FK_mhl_cities_Commune_ID
 FOREIGN KEY (commune_ID) REFERENCES mhl_communes(id);
 -- Fails
+	
+    -- Checken waar t mis gaat:
+    SELECT * FROM mhl_cities
+	LEFT JOIN mhl_communes ON mhl_cities.commune_ID = mhl_communes.id
+	WHERE mhl_communes.id IS NULL;
 
 	ALTER TABLE mhl_cities MODIFY commune_ID INT(11) NULL;
     
@@ -38,26 +45,50 @@ FOREIGN KEY (commune_ID) REFERENCES mhl_communes(id);
 	SET commune_ID=NULL
 	WHERE commune_ID=0;
 	--  Still nope
+    
+    -- ER is ook nog een niet bekende commune_ID 755:
+    DELETE FROM mhl_cities WHERE commune_ID = 755;
+    -- En dan werkt tie wel!
+
+-- 2. mhl_communes EN mhl_districts ==========================================
 
 ALTER TABLE mhl_communes
 ADD CONSTRAINT FK_District_ID
 FOREIGN KEY (district_ID) REFERENCES mhl_districts(id);
 -- Success
 
+-- 3. mhl_contracts & mhl_departments ===========================================
+
 ALTER TABLE mhl_contacts
 ADD CONSTRAINT FK_mhl_contacts_department
 FOREIGN KEY (department) REFERENCES mhl_departments(id);
 -- Success
+
+-- 4. mhl_contacts & mhl_suppliers ================================================
 
 ALTER TABLE mhl_contacts
 ADD CONSTRAINT FK_mhl_contacts_supplier_ID
 FOREIGN KEY (supplier_ID) REFERENCES mhl_suppliers(id);
 -- Fails
 
+    ALTER TABLE mhl_contacts MODIFY supplier_ID INT(11) NULL;
+
+	UPDATE mhl_contacts
+	SET supplier_ID = NULL
+	WHERE supplier_ID IN(
+		SELECT supplier_ID FROM mhl_contacts
+		LEFT JOIN mhl_suppliers ON mhl_contacts.supplier_ID = mhl_suppliers.id
+		WHERE mhl_suppliers.id IS NULL);
+	-- Nu Werkt tie
+   
+-- 5. mhl_detaildefs & detailgroups =================================================
+
 ALTER TABLE mhl_detaildefs
 ADD CONSTRAINT FK_mhl_detailsdefs_group_ID
 FOREIGN KEY (group_ID) REFERENCES mhl_detailgroups(id);
 -- success
+
+-- 6. mhl_detaildefs & mhl+propertytype_ID ===============================================
 
 ALTER TABLE mhl_detaildefs
 ADD CONSTRAINT FK_mhl_detailsdefs_propertytype_ID
@@ -71,15 +102,61 @@ FOREIGN KEY (propertytype_ID) REFERENCES mhl_propertytypes(id);
 	WHERE propertytype_ID=0;
 	-- En nu werkt de add foreign key statment hierboven wel
     
+-- 7. mhl_districts & mhl_countries ===========================================================
+    
 ALTER TABLE mhl_districts
 ADD CONSTRAINT FK_mhl_districts_country_ID
 FOREIGN KEY (country_ID) REFERENCES mhl_countries(id);
 -- Success
 
+-- 8. mhl_properties & mhl_suppliers ===========================================================
+
 ALTER TABLE mhl_properties
 ADD CONSTRAINT FK_mhl_properties_supplier_ID
 FOREIGN KEY (supplier_ID) REFERENCES mhl_suppliers(id);
 -- Error Code: 1452. Cannot add or update a child row: a foreign key constraint fails (`mhl`.`#sql-8f8_5d`, CONSTRAINT `FK_mhl_properties_supplier_ID` FOREIGN KEY (`supplier_ID`) REFERENCES `mhl_suppliers` (`id`))
+
+SELECT * FROM mhl_properties
+LEFT JOIN mhl_suppliers ON mhl_properties.supplier_ID = mhl_suppliers.id
+WHERE mhl_suppliers.id IS NULL;
+
+ALTER TABLE mhl_properties MODIFY supplier_ID INT(11) NULL;
+
+CREATE TABLE parent (
+   id int(10) UNSIGNED NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=INNODB;
+CREATE TABLE child (
+    id int(10) UNSIGNED NOT NULL,
+    parent_id int(10) UNSIGNED DEFAULT NULL,
+    FOREIGN KEY (parent_id) REFERENCES parent(id) ON DELETE CASCADE
+) ENGINE=INNODB;
+
+USE testDBForeignKeys;
+
+CREATE TABLE mhl_suppliers (
+	id INT(11) UNSIGNED NOT NULL,
+    membertype INT(11),
+    company INT(11),
+    name VARCHAR(75) NULL,
+    straat VARCHAR(50) NULL,
+    huisnr VARCHAR(25) NULL,
+    postcode VARCHAR(7) NULL,
+    city_ID INT(11) NULL,
+    p_address VARCHAR(30) NULL,
+    p_postcode VARCHAR(7) NULL,
+    p_city_ID INT(11) NULL,
+    PRIMARY KEY (id)
+    ) ENGINE=INNODB;
+CREATE TABLE mhl_properties (
+	id INT(11) UNSIGNED NOT NULL,
+    supplier_ID INT(11) UNSIGNED DEFAULT NULL,
+    propertytype_ID INT(11),
+    content TEXT,
+    FOREIGN KEY(supplier_ID) REFERENCES mhl_suppliers(id) ON DELETE CASCADE
+    ) ENGINE=INNODB;
+
+-- 9.
 
 ALTER TABLE mhl_properties
 ADD CONSTRAINT FK_mhl_properties_propertytype_ID
